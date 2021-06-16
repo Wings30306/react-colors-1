02fbb4e99a9d4c45d7ac18adee6d2f26176acbfd
Joke Heyndels <jo_hannah@outlook.com>
2021-06-16 12:43:04 +0000

    Add Routing

--- colors/src/App.js
+++ colors/src/App.js
@@ -1,11 +1,17 @@
 import { generatePalette } from './colorHelpers';
+import 'react-router-dom'
 import Palette from './Palette';
 import seedColors from "./seedColors"
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
     return (
     <div className="App">
-      <Palette palette={generatePalette(seedColors[0])} />
+      <Switch>
+        <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>}/>
+        <Route exact path="/palette/:id" render={() => <Palette palette={generatePalette(seedColors[4])} />}/>
+      </Switch>
+      
     </div>
   );
 }
