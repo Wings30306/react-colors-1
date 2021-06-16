26682a6078209de18170f695c4ae15ee4057ce9e
Joke Heyndels <jo_hannah@outlook.com>
2021-06-16 12:59:00 +0000

    Fix Palette Route (make dynamic)

--- colors/src/App.js
+++ colors/src/App.js
@@ -3,17 +3,32 @@ import 'react-router-dom'
 import Palette from './Palette';
 import seedColors from "./seedColors"
 import { Route, Switch } from 'react-router-dom';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id
+    })
+  }
+
+  render() {
     return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>}/>
-        <Route exact path="/palette/:id" render={() => <Palette palette={generatePalette(seedColors[4])} />}/>
-      </Switch>
-      
-    </div>
-  );
+      <div className="App">
+        <Switch>
+          <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+          <Route 
+            exact 
+            path="/palette/:id" 
+            render={(routeProps) => (
+              <Palette palette={generatePalette(
+                this.findPalette(routeProps.match.params.id))} />)} />
+        </Switch>
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
