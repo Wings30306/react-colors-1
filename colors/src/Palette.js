f63482bf6af278799aca5672d7350f21fd455fef
Joke Heyndels <jo_hannah@outlook.com>
2021-06-16 10:22:09 +0000

    Add Select from Material-UI for format change

--- colors/src/Palette.js
+++ colors/src/Palette.js
@@ -8,23 +8,28 @@ import "./Palette.css";
 class Palette extends Component {
     constructor(props){
         super(props);
-        this.state = { level: 500}
-        this.changeLevel = this.changeLevel.bind(this)
+        this.state = { level: 500, format: "hex"}
+        this.changeLevel = this.changeLevel.bind(this);
+        this.changeFormat = this.changeFormat.bind(this);
     }
 
     changeLevel(level){
         this.setState({ level })
     }
 
+    changeFormat(val){
+        this.setState({ format: val})
+    }
+
     render() {
         const { colors } = this.props.palette;
-        const { level } = this.state;
+        const { level, format } = this.state;
         const ColorBoxes = colors[level].map(color => (
-            <ColorBox color={color.hex} name={color.name} />
+            <ColorBox color={color[format]} name={color.name} />
         ))
         return (
             <div className="Palette">               
-                <Navbar level={level} changeLevel={this.changeLevel}/>
+                <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat}/>
                 <div className="Palette-colors">
                     {ColorBoxes}
                 </div>
