2afdd8529fb83a6a3f81b317e03457c314dbd10f
Joke Heyndels <jo_hannah@outlook.com>
2021-07-07 15:11:35 +0000

    Add PaletteFormModal

---
+++ colors/src/PaletteFormModal.js
@@ -0,0 +1,86 @@
+import { Component } from "react";
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
+
+class PaletteFormModal extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            open: false,
+            newPaletteName: ""
+        }
+        this.handleClickOpen = this.handleClickOpen.bind(this);
+        this.handleClose = this.handleClose.bind(this)
+        this.handleChange = this.handleChange.bind(this);
+    }
+    
+    handleChange(evt) {
+      this.setState({
+        [evt.target.name]: evt.target.value
+      });
+    }
+
+    componentDidMount() {
+        ValidatorForm.addValidationRule("isPaletteNameUnique", value =>
+          this.props.palettes.every(
+            ({ paletteName }) => paletteName.toLowerCase() !== value.toLowerCase()
+          )
+        );
+      }
+
+    handleClickOpen(){
+        this.setState({ open: true });
+    };
+
+    handleClose(){
+        this.setState({ open: false });
+    };
+
+    
+
+    render() {
+        return (
+            <div>
+                <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
+                    Open form dialog
+                </Button>
+                <Dialog
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                    aria-labelledby="form-dialog-title"
+                >
+                    <DialogTitle id="form-dialog-title">Name your palette</DialogTitle>
+                    <DialogContent>
+                        <ValidatorForm
+                            onSubmit={() => this.props.handleSubmit(this.state.newPaletteName)}
+                        >
+                            <TextValidator
+                                label='Palette Name'
+                                value={this.state.newPaletteName}
+                                name='newPaletteName'
+                                onChange={this.handleChange}
+                                validators={["required", "isPaletteNameUnique"]}
+                                errorMessages={["Enter Palette Name", "Name already used"]}
+                            />
+                            <Button variant='contained' color='primary' type='submit'>
+                                Save Palette
+                            </Button>
+                        </ValidatorForm>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            Cancel
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
+}
+
+export default PaletteFormModal
--- colors/src/PaletteFormNav.js
+++ colors/src/PaletteFormNav.js
@@ -10,7 +10,7 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
-import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import PaletteFormModal from "./PaletteFormModal";
 
 const drawerWidth = 400;
 const styles = theme => ({
@@ -47,13 +47,7 @@ class PaletteFormNav extends Component {
     this.state = { newPaletteName: "" };
     this.handleChange = this.handleChange.bind(this);
   }
-  componentDidMount() {
-    ValidatorForm.addValidationRule("isPaletteNameUnique", value =>
-      this.props.palettes.every(
-        ({ paletteName }) => paletteName.toLowerCase() !== value.toLowerCase()
-      )
-    );
-  }
+  
   handleChange(evt) {
     this.setState({
       [evt.target.name]: evt.target.value
@@ -61,7 +55,6 @@ class PaletteFormNav extends Component {
   }
   render() {
     const { classes, open } = this.props;
-    const { newPaletteName } = this.state;
     return (
       <div className={classes.root}>
         <CssBaseline />
@@ -86,21 +79,10 @@ class PaletteFormNav extends Component {
             </Typography>
           </Toolbar>
           <div className={classes.navBtns}>
-            <ValidatorForm
-              onSubmit={() => this.props.handleSubmit(newPaletteName)}
-            >
-              <TextValidator
-                label='Palette Name'
-                value={this.state.newPaletteName}
-                name='newPaletteName'
-                onChange={this.handleChange}
-                validators={["required", "isPaletteNameUnique"]}
-                errorMessages={["Enter Palette Name", "Name already used"]}
-              />
-              <Button variant='contained' color='primary' type='submit'>
-                Save Palette
-              </Button>
-            </ValidatorForm>
+            <PaletteFormModal 
+             handleSubmit={this.props.savePalette} 
+             palettes={this.props.palettes}
+            />
             <Link to='/'>
               <Button variant='contained' color='secondary'>
                 Go Back
