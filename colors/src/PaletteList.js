7502f233b67120d4a7378c78fdf14dc5040bafcf
Joke Heyndels <jo_hannah@outlook.com>
2021-07-14 10:08:32 +0000

    animate palette deletion

--- colors/src/PaletteList.js
+++ colors/src/PaletteList.js
@@ -1,36 +1,38 @@
 import { Component } from "react";
 import { Link } from "react-router-dom"
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { withStyles } from "@material-ui/styles"
 import MiniPalette from "./MiniPalette";
 import styles from "./styles/PaletteListStyles"
 
 class PaletteList extends Component {
-    goToPalette(id){
+    goToPalette(id) {
         this.props.history.push(`/palette/${id}`)
     }
     render() {
         const { palettes, classes } = this.props;
         return (
             <div className={classes.root}>
-                
                 <div className={classes.container}>
                     <nav className={classes.nav}>
                         <h1>React Colours</h1>
                         <Link to="/palette/new">Create new palette</Link>
                     </nav>
-                    <div className={classes.palettes}>
+
+                    <TransitionGroup className={classes.palettes}>
                         {palettes.map(palette => (
-                        <MiniPalette 
-                            key={palette.id} 
-                            {...palette} 
-                            handleClick={() => this.goToPalette(palette.id)}
-                            deletePalette={this.props.deletePalette}
-                        />
-                        ))}
-                    </div>
-                    
+                            <CSSTransition key={palette.id} classNames="fade" timeout={500}>
+                                <MiniPalette
+                                    key={palette.id}
+                                    {...palette}
+                                    handleClick={() => this.goToPalette(palette.id)}
+                                    deletePalette={this.props.deletePalette}
+                                />
+                            </CSSTransition>))
+                        }
+                    </TransitionGroup>
+
                 </div>
-                
             </div>
         )
     }
